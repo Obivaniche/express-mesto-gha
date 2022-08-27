993e34cb21377a5afab9be3d31d2296fa9dc2827
Vladislav <obivaniche@yandex.ru>
2022-08-27 18:53:46 +0300

    ПР 13
    Добавлены обработчики ошибок карточек пользователей

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,33 @@
 const User = require('../models/user');
 
+const {
+  BAD_REQUEST_STATUS,
+  NOT_FOUND_STATUS,
+  SERVER_ERROR_STATUS,
+} = require('../utils/errors');
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+        return;
+      }
+      res.status(NOT_FOUND_STATUS).send({ message: 'Пользователь не найден' });
+    })
+    .catch((err) => {
+      if (err.name === 'castError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректный запрос' });
+        return;
+      }
+      res.status(SERVER_ERROR_STATUS).send({ message: err.name });
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -17,19 +35,65 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректные данные' });
+        return;
+      }
+      res.status(SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+    });
 };
 
 module.exports.updateUser = (req, res) => {
-  User.findByIdAndUpdate(req.user._id, { name: req.body.name, about: req.body.about })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name: req.body.name, about: req.body.about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+        return;
+      }
+      res.status(NOT_FOUND_STATUS).send({ message: 'Пользователь не найден' });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректные данные' });
+        return;
+      }
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректный запрос' });
+        return;
+      }
+      res.status(SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+        return;
+      }
+      res.status(NOT_FOUND_STATUS).send({ message: 'Пользователь не найден' });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректные данные' });
+        return;
+      }
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректный запрос' });
+        return;
+      }
+      res.status(SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
+    });
 };
