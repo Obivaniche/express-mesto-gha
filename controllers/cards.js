14573f125011bb6a966f7f5a13b429d84039b2b2
Vladislav <obivaniche@yandex.ru>
2022-08-29 13:42:29 +0300

    ПР 13

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,25 +30,17 @@ module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (card) {
-        res.send({
-          data: card,
-        });
+        res.send({ data: card });
         return;
       }
-      res.status(NOT_FOUND_STATUS).send({
-        message: 'Карточка с таким id не найдена',
-      });
+      res.status(NOT_FOUND_STATUS).send({ message: 'Карточка не найдена' });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_STATUS).send({
-          message: 'Некорректный id карточки',
-        });
+        res.status(BAD_REQUEST_STATUS).send({ message: 'Некорректный запрос' });
         return;
       }
-      res.status(SERVER_ERROR_STATUS).send({
-        message: 'На сервере произошла ошибка',
-      });
+      res.status(SERVER_ERROR_STATUS).send({ message: 'Ошибка сервера' });
     });
 };
 
