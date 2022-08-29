b17ed8fa1f44fa3a04e47fdf5cd7facd14aaed20
Vladislav <obivaniche@yandex.ru>
2022-08-29 15:01:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/users'));
